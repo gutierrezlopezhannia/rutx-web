4fc25e3aeccc8dcb6bd45b3f33c7c950acbf8a2e
estr05 <estradaperezneyser@gmail.com>
2026-07-24 13:06:51 -0600

    feat: lementacion de existencias globales, en el modulo de inventarios, se añidio la vista en el sliderbar

--- routes/modules/inventario.php
+++ routes/modules/inventario.php
@@ -10,4 +10,5 @@ Route::middleware(['auth', 'verified'])->prefix('inventario')->name('inventario.
     Volt::route('/mermas', 'inventario.mermas')->name('mermas');
     Volt::route('/ruta', 'inventario.ruta')->name('ruta');
     Volt::route('/entrada-almacen', 'inventario.entrada-almacen')->name('entrada-almacen');
+    Volt::route('/existencias-globales', 'inventario.existencias-globales')->name('existencias-globales');
 });
